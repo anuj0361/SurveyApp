f6137af553630cf17b092fe55a5fdf3d76478806
unknown <anujpandey1630@gmail.com>
2018-12-04 23:33:56 +0530

    Survey Complete

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -19,6 +19,15 @@ module.exports = app => {
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
